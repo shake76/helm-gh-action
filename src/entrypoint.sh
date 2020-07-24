d95710fa13f62f9e6fe5f7f06e15d361d2a553bf
shake76 <carbafer@gmail.com>
2020-07-23 23:37:23 -0300

    adding another test

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -41,7 +41,7 @@ main() {
   fi
 
   if [[ -z "$REPO_URL" ]]; then
-      REPO_URL="https://x-access-token:${GITHUB_TOKEN}@github.com/${OWNER}/${REPOSITORY}"
+      REPO_URL="https://x-access-token:${GITHUB_TOKEN}@github.com/${OWNER}/helm-carts/${REPOSITORY}"
   fi
 
   download
